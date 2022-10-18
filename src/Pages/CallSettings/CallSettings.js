e63936e3cff5ee89429087d253a00a6b73d9fd12
Cameron McGuffie <cameron.mcguffie@pexip.com>
2022-10-18 10:51:41 +1000

    Make Media Selection a Component

--- src/Pages/CallSettings/CallSettings.js
+++ src/Pages/CallSettings/CallSettings.js
@@ -1,32 +1,13 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import MediaSelection from '../../Components/MediaSelection/MediaSelection';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faGear,
-  faVideo,
-  faChevronUp,
-  faChevronDown,
-  faMicrophone,
-} from '@fortawesome/free-solid-svg-icons';
-import { usePexip } from '../../Providers/Pexip/PexipProvider';
+import { faGear } from '@fortawesome/free-solid-svg-icons';
 
 import './CallSettings.css';
 
 export default function CallSettings() {
-  const [videoOpen, setVideoOpen] = useState(false);
-  const [audioOpen, setAudioOpen] = useState(false);
-  const [videoDevices, setVideoDevices] = useState([]);
-  const [audioDevices, setAudioDevices] = useState([]);
-  const [selectedAudioDeviceLabel, setSelectedAudioDeviceLabel] = useState([]);
-  const [selectedVideoDeviceLabel, setSelectedVideoDeviceLabel] = useState([]);
   const [callSettingsOpen, setCallSettingsOpen] = useState(false);
-
-  const {
-    updateCurrentPage,
-    selectAudioDevice,
-    selectedAudioDevice,
-    selectVideoDevice,
-    selectedVideoDevice,
-  } = usePexip();
 
   function toggleCallSettings() {
     setCallSettingsOpen(!callSettingsOpen);
@@ -35,70 +16,6 @@ export default function CallSettings() {
   function closeCallSettings() {
     setCallSettingsOpen(false);
   }
-
-  function toggleVideo() {
-    setAudioOpen(false);
-    setVideoOpen(!videoOpen);
-  }
-
-  function toggleAudio() {
-    setVideoOpen(false);
-    setAudioOpen(!audioOpen);
-  }
-
-  useEffect(() => {
-    // Set the constraints of the video to search for
-    // https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
-    let constraints = {
-      video: {
-        height: {
-          min: 128,
-        },
-        width: {
-          min: 128,
-        },
-      },
-      audio: true,
-    };
-
-    // An async function to get the video and audio devices
-    async function getMediaDevices(constraints) {
-      // Request permission to list devices
-      await navigator.mediaDevices.getUserMedia(constraints);
-      // Enumerate the devices
-      let devices = await navigator.mediaDevices.enumerateDevices();
-
-      // Filter only video devices
-      setVideoDevices(devices.filter((d) => d.kind === 'videoinput'));
-      // Filter only audio devices
-      setAudioDevices(devices.filter((d) => d.kind === 'audioinput'));
-    }
-
-    // Run the async function
-    getMediaDevices(constraints);
-  }, []);
-
-  useEffect(() => {
-    if (videoDevices.length > 0) {
-      setSelectedVideoDeviceLabel(
-        selectedVideoDevice
-          ? videoDevices.filter((d) => d.deviceId === selectedVideoDevice)[0]
-              .label
-          : videoDevices[0].label
-      );
-    }
-  }, [videoDevices, selectedVideoDevice]);
-
-  useEffect(() => {
-    if (audioDevices.length > 0) {
-      setSelectedAudioDeviceLabel(
-        selectedAudioDevice
-          ? audioDevices.filter((d) => d.deviceId === selectedAudioDevice)[0]
-              .label
-          : audioDevices[0].label
-      );
-    }
-  }, [audioDevices, selectedAudioDevice]);
 
   return (
     <>
@@ -115,71 +32,8 @@ export default function CallSettings() {
       >
         <div className='popupTitle'>Call settings</div>
         <div className='popupSeperator'></div>
-        <div className='mediaSelection'>
-          <div
-            className='mediaItem mediaTop'
-            onClick={() => {
-              toggleVideo();
-            }}
-          >
-            <div className='mediaIcon'>
-              <FontAwesomeIcon icon={faVideo} />
-            </div>
-            <div className='mediaName'>{selectedVideoDeviceLabel}</div>
-            <div className='mediaCaret'>
-              {videoOpen ? (
-                <FontAwesomeIcon icon={faChevronUp} />
-              ) : (
-                <FontAwesomeIcon icon={faChevronDown} />
-              )}
-            </div>
-          </div>
 
-          {videoDevices.map((device) => {
-            return (
-              <div
-                className='mediaSubItem'
-                style={{ display: videoOpen ? 'block' : 'none' }}
-                key={device.deviceId}
-                onClick={() => selectVideoDevice(device.deviceId)}
-              >
-                <div className='mediaName'>{device.label}</div>
-              </div>
-            );
-          })}
-
-          <div
-            className='mediaItem mediaBottom'
-            onClick={() => {
-              toggleAudio();
-            }}
-          >
-            <div className='mediaIcon'>
-              <FontAwesomeIcon icon={faMicrophone} />
-            </div>
-            <div className='mediaName'>{selectedAudioDeviceLabel}</div>
-            <div className='mediaCaret'>
-              {audioOpen ? (
-                <FontAwesomeIcon icon={faChevronUp} />
-              ) : (
-                <FontAwesomeIcon icon={faChevronDown} />
-              )}
-            </div>
-          </div>
-
-          {audioDevices.map((device) => {
-            return (
-              <div
-                className='mediaSubItem'
-                style={{ display: audioOpen ? 'block' : 'none' }}
-                key={device.deviceId}
-                onClick={() => selectAudioDevice(device.deviceId)}
-              >
-                <div className='mediaName'>{device.label}</div>
-              </div>
-            );
-          })}
-        </div>
+        <MediaSelection />
 
         <div className='buttons'>
           <div className='leftbutton'></div>
--- src/Pages/MediaSetup/MediaSetup.js
+++ src/Pages/MediaSetup/MediaSetup.js
@@ -1,112 +1,32 @@
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { usePexip } from '../../Providers/Pexip/PexipProvider';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faVideo,
-  faChevronUp,
-  faChevronDown,
-  faMicrophone,
-} from '@fortawesome/free-solid-svg-icons';
+import MediaSelection from '../../Components/MediaSelection/MediaSelection';
 
 import './MediaSetup.css';
 
 export default function MediaSetup() {
-  const [videoOpen, setVideoOpen] = useState(false);
-  const [audioOpen, setAudioOpen] = useState(false);
-  const [videoDevices, setVideoDevices] = useState([]);
-  const [audioDevices, setAudioDevices] = useState([]);
-  const [selectedAudioDeviceLabel, setSelectedAudioDeviceLabel] = useState([]);
-  const [selectedVideoDeviceLabel, setSelectedVideoDeviceLabel] = useState([]);
   const nearEndVideo = useRef(null);
-  const {
-    updateCurrentPage,
-    selectAudioDevice,
-    selectedAudioDevice,
-    selectVideoDevice,
-    selectedVideoDevice,
-  } = usePexip();
+  const { updateCurrentPage, selectedVideoDevice } = usePexip();
+
+  function setupSelfView() {
+    navigator.mediaDevices
+      .getUserMedia({
+        video: {
+          deviceId: selectedVideoDevice,
+        },
+      })
+      .then(function success(stream) {
+        nearEndVideo.current.srcObject = stream;
+      });
+  }
 
   useEffect(() => {
-    // Set the constraints of the video to search for
-    // https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
-    let constraints = {
-      video: {
-        height: {
-          min: 128,
-        },
-        width: {
-          min: 128,
-        },
-      },
-      audio: true,
-    };
+    setupSelfView();
+  }, [selectedVideoDevice]);
 
-    // An async function to get the video and audio devices
-    async function getMediaDevices(constraints) {
-      // Request permission to list devices
-      await navigator.mediaDevices.getUserMedia(constraints);
-      // Enumerate the devices
-      let devices = await navigator.mediaDevices.enumerateDevices();
-
-      // Filter only video devices
-      setVideoDevices(devices.filter((d) => d.kind === 'videoinput'));
-      // Filter only audio devices
-      setAudioDevices(devices.filter((d) => d.kind === 'audioinput'));
-
-      console.log(
-        'HERE2',
-        devices.filter((d) => d.kind === 'videoinput')
-      );
-    }
-
-    // Run the async function
-    getMediaDevices(constraints);
+  useEffect(() => {
+    setupSelfView();
   }, []);
-
-  useEffect(() => {
-    if (videoDevices.length > 0) {
-      setSelectedVideoDeviceLabel(
-        selectedVideoDevice
-          ? videoDevices.filter((d) => d.deviceId === selectedVideoDevice)[0]
-              .label
-          : videoDevices[0].label
-      );
-
-      navigator.mediaDevices
-        .getUserMedia({
-          video: {
-            deviceId: selectedVideoDevice
-              ? selectedVideoDevice
-              : videoDevices[0].deviceId,
-          },
-        })
-        .then(function success(stream) {
-          nearEndVideo.current.srcObject = stream;
-        });
-    }
-  }, [videoDevices, selectedVideoDevice]);
-
-  useEffect(() => {
-    if (audioDevices.length > 0) {
-      setSelectedAudioDeviceLabel(
-        selectedAudioDevice
-          ? audioDevices.filter((d) => d.deviceId === selectedAudioDevice)[0]
-              .label
-          : audioDevices[0].label
-      );
-    }
-  }, [audioDevices, selectedAudioDevice]);
-
-  function toggleVideo() {
-    setAudioOpen(false);
-    setVideoOpen(!videoOpen);
-  }
-
-  function toggleAudio() {
-    setVideoOpen(false);
-    setAudioOpen(!audioOpen);
-  }
 
   function nextPage() {
     updateCurrentPage('MEETING_DETAILS');
@@ -120,71 +40,8 @@ export default function MediaSetup() {
         <div className='videoPreview'>
           <video ref={nearEndVideo} autoPlay='autoplay' muted></video>
         </div>
-        <div className='mediaSelection'>
-          <div
-            className='mediaItem mediaTop'
-            onClick={() => {
-              toggleVideo();
-            }}
-          >
-            <div className='mediaIcon'>
-              <FontAwesomeIcon icon={faVideo} />
-            </div>
-            <div className='mediaName'>{selectedVideoDeviceLabel}</div>
-            <div className='mediaCaret'>
-              {videoOpen ? (
-                <FontAwesomeIcon icon={faChevronUp} />
-              ) : (
-                <FontAwesomeIcon icon={faChevronDown} />
-              )}
-            </div>
-          </div>
 
-          {videoDevices.map((device) => {
-            return (
-              <div
-                className='mediaSubItem'
-                style={{ display: videoOpen ? 'block' : 'none' }}
-                key={device.deviceId}
-                onClick={() => selectVideoDevice(device.deviceId)}
-              >
-                <div className='mediaName'>{device.label}</div>
-              </div>
-            );
-          })}
-
-          <div
-            className='mediaItem mediaBottom'
-            onClick={() => {
-              toggleAudio();
-            }}
-          >
-            <div className='mediaIcon'>
-              <FontAwesomeIcon icon={faMicrophone} />
-            </div>
-            <div className='mediaName'>{selectedAudioDeviceLabel}</div>
-            <div className='mediaCaret'>
-              {audioOpen ? (
-                <FontAwesomeIcon icon={faChevronUp} />
-              ) : (
-                <FontAwesomeIcon icon={faChevronDown} />
-              )}
-            </div>
-          </div>
-
-          {audioDevices.map((device) => {
-            return (
-              <div
-                className='mediaSubItem'
-                style={{ display: audioOpen ? 'block' : 'none' }}
-                key={device.deviceId}
-                onClick={() => selectAudioDevice(device.deviceId)}
-              >
-                <div className='mediaName'>{device.label}</div>
-              </div>
-            );
-          })}
-        </div>
+        <MediaSelection />
 
         <div className='buttons'>
           <div className='leftbutton'></div>
